68830d270de513b3dd4f50ea25b156fdc7acc964
Nastaran <nastaran@MacBook-Pro.local>
2025-09-27 15:25:30 +0330

    Q1 queries added

--- Query.sql
+++ Query.sql
@@ -1,13 +1,11 @@
 -- Question 1, query 1
+
     SELECT mvp.player_name, mvp.height
     FROM mvp;
 
 -- Question 1, query 2
-    WITH best_50 AS (
-        SELECT player_name, height, total_point
-        FROM best_player
-    )
-    SELECT *
-    FROM best_50
+
+    SELECT player_name, height, total_point
+    FROM best_player
     ORDER BY total_point DESC
     LIMIT 50;
